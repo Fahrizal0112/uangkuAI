b286d0550f0b512988961ef5b9156d12a0064092
fahrizal <muchamad.fachrizal12@gmail.com>
2025-01-23 14:40:47 +0700

    fix origin

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ const routes = require('./routes/routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['*'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
